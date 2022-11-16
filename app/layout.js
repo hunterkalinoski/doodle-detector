bce98431f21b746cb4b3f6fc8aa0a19b33198a9c
Hunter Kalinoski <hunterkalinoski@gmail.com>
2022-11-16 18:55:41 -0500

    fixed mobile media query

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,9 @@ import "../styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   );
