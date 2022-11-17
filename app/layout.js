6cdea83c9257aa616fb6c8d31b973e1d10dec3a6
Hunter Kalinoski <hunterkalinoski@gmail.com>
2022-11-16 19:04:20 -0500

    removed viewport meta tag prevents mobile from opening the site zoomed in extremely far

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       </head>
       <body>{children}</body>
     </html>
